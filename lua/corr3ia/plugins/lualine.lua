10b6eec70edef037731b02981a1e68f5c4420260
Luciano Correia <luciano.correia@gmail.com>
2024-05-22 14:17:39 +0400

    update config

--- lua/corr3ia/plugins/lualine.lua
+++ lua/corr3ia/plugins/lualine.lua
@@ -6,8 +6,8 @@ return {
             -- sections = {lualine_c = {"filename", {getWords}}, lualine_x = {{getGuiFont}, 'filetype'}},
             options = {
                 icons_enabled = true,
-                -- theme = 'everforest',
-                theme = 'catppuccin',
+                theme = 'rose-pine',
+                -- theme = 'catppuccin',
                 component_separators = '|',
                 section_separators = '',
             },
