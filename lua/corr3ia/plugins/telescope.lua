0371034aaeb0513c4c955b6feb736a3587a22a98
Luciano Correia <luciano.correia@gmail.com>
2024-06-01 12:23:19 +0400

    update config

--- lua/corr3ia/plugins/telescope.lua
+++ lua/corr3ia/plugins/telescope.lua
@@ -12,15 +12,15 @@ return {
         local actions = require("telescope.actions")
         local transform_mod = require("telescope.actions.mt").transform_mod
 
-        local trouble = require("trouble")
-        local trouble_telescope = require("trouble.providers.telescope")
-
-        -- or create your custom action
-        local custom_actions = transform_mod({
-            open_trouble_qflist = function(prompt_bufnr)
-                trouble.toggle("quickfix")
-            end,
-        })
+        -- local trouble = require("trouble")
+        -- local trouble_telescope = require("trouble.sources.telescope")
+        --
+        -- -- or create your custom action
+        -- local custom_actions = transform_mod({
+        --     open_trouble_qflist = function(prompt_bufnr)
+        --         trouble.toggle("quickfix")
+        --     end,
+        -- })
 
         telescope.setup({
             defaults = {
@@ -29,8 +29,8 @@ return {
                     i = {
                         ["<C-k>"] = actions.move_selection_previous, -- move to prev result
                         ["<C-j>"] = actions.move_selection_next,     -- move to next result
-                        ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-                        ["<C-t>"] = trouble_telescope.open(),
+                        -- ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+                        -- ["<C-t>"] = require("trouble.sources.telescope").open(),
                     },
                 },
 
